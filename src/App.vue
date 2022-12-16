55ad32a4a3e31ec68cae2797fbeebf5ffa03ac8a
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2022-12-16 09:40:20 +0100

    .

--- src/App.vue
+++ src/App.vue
@@ -21,10 +21,6 @@ export default {
   @import url('https://fonts.googleapis.com/css27family=Roboto:wght@300:400:700&display=swap');
 #app {
   font-family: 'Roboto', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
--- src/components/RestaurentCard.vue
+++ src/components/RestaurentCard.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="restaurant--card">
-    <p>test</p>
+    <div class="restaurent--image">
+
+    </div>
+    <div class="restaurent--informations">
+      <div class="top">
+        <p class="name">
+          Subway
+        </p>
+        <p class="note">
+          <span>
+            4.5
+          </span>
+        </p>
+      </div>
+      <p class="time">
+        20-30mn
+      </p>
+    </div>
   </div>
 </template>
 
@@ -14,10 +31,36 @@ export default {
 .restaurant--card{
     width: 32%;
     height: 30vh;
-
   p {
     margin: 0px;
-  }    
+  }  
+  .restaurent--image {
+    background-image: url("https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8zNzg4MDJiMC1jNTI4LTQ4MjktYjBiNS0wY2M2NDBkZjYzY2QuanBlZw==");
+    height: 70%;
+    background-size: cover;
+    background-position: center;
+  }
+  .restaurent--informations {
+    .top {
+      margin: 10px 0px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      .note {
+        background-color: #eeeeee;
+        height: 30px;
+        width: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 0.8rem;
+        border-radius: 50%;
+      }
+    }
+    .time {
+      font-size: 0.9rem;
+    }
+  }
 }
 
 
