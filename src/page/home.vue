c43917ad2ce6eb33a38ff1892680094d17b2a49e
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2022-12-16 16:57:50 +0100

    7

--- src/page/home.vue
+++ src/page/home.vue
@@ -6,27 +6,46 @@
 
 <script>
 //IMPORT
-// import BDD from '../BDD.js';
+import BDD from '../BDD.js'
+import { onMounted } from 'vue'
 //COMPONENTS
 import RestaurantRowVue from '@/components/RestaurantRow.vue';
+
 export default {
     name: 'HomePage',
     components: {
         RestaurantRowVue,
     },
     setup() {
-    class Restaurant {
-        constructor(name, note, image, drive_time) {
-            this.name = name
-            this.note = note
-            this.image = image
-            this.drive_time = drive_time
+        class Restaurant {
+            constructor(name, note, image, drive_time) {
+                this.name = name
+                this.note = note
+                this.image = image
+                this.drive_time = drive_time
+            }
         }
-    }
 
-    var resto = new Restaurant('restaurant lechat', 4.5, 'img', '45mn')
-    console.log(resto)
-}
+        let data_restaurant = []
+
+        const makeDataRestaurant = () => {
+            let three_restaurant = [];
+            
+            for (const restaurant of BDD) {
+                const new_restaurant = new Restaurant(restaurant.name, restaurant.note, restaurant.image, restaurant.drive_time)
+
+                if (three_restaurant.length === 2) {
+                    three_restaurant.push(new_restaurant);
+                    data_restaurant.push(three_restaurant);
+                    three_restaurant = [];
+                } else {
+                    three_restaurant.push(new_restaurant);  
+                }
+            }
+        }
+
+        onMounted(makeDataRestaurant);
+    },
 }
 </script>
 
